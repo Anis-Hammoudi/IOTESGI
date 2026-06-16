f4364403e669e95313c018aaee04ccaeee9725e0
Anis-Hammoudi <94543307+animaSPW@users.noreply.github.com>
2026-06-16 15:53:28 +0200

    feat: add project dependencies and initialize PlatformIO build environment

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/cheda/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/cheda/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/esp32dev/lib77f/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib77f/WebServer/detail/mimetable.cpp.o: \
- C:/Users/cheda/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/cheda/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/cheda/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
